00c793a52bac758faa8fdd3a69f7c794db03e263
Aaron DeGrow <adegrow@ford.com>
2015-01-30 18:38:54 -0500

    Removed more unnecessary code for a simple drive train robot

--- src/robot/robot.py
+++ src/robot/robot.py
@@ -13,8 +13,6 @@ import drivetrain
 import logging
 import math
 import parameters
-import queue
-import stopwatch
 import sys
 import userinterface
 
@@ -35,7 +33,6 @@ class MyRobot(wpilib.SimpleRobot):
     _log = None
     _logger = None
     _parameters = None
-    _timer = None
     _user_interface = None
 
     # Private parameters
@@ -45,9 +42,6 @@ class MyRobot(wpilib.SimpleRobot):
     _parameters_file = None
     _driver_alternate = False
     _driver_controls_swap_ratio = 1.0
-    _robot_names = None
-    _drive_train_names = None
-    _user_interface_names = None
 
     def _initialize(self, params, logging_enabled):
         """Initialize the robot.
@@ -77,7 +71,6 @@ class MyRobot(wpilib.SimpleRobot):
         self._drive_train = None
         self._log = None
         self._parameters = None
-        self._timer = None
         self._user_interface = None
 
         # Initialize private parameters
@@ -87,16 +80,6 @@ class MyRobot(wpilib.SimpleRobot):
         self._parameters_file = None
         self._driver_alternate = False
         self._driver_controls_swap_ratio = 1.0
-
-        # Enable logging if specified
-        #if logging_enabled:
-            # Create a new data log object
-            #self._log = datalog.DataLog("robot.log")
-
-            #if self._log and self._log.file_opened:
-                #self._log_enabled = True
-
-        self._timer = stopwatch.Stopwatch()
 
         # Read parameters file
         self._parameters_file = params
@@ -108,11 +91,6 @@ class MyRobot(wpilib.SimpleRobot):
         self._user_interface = userinterface.UserInterface(
                                                     "/py/par/userinterface.par",
                                                     self._log_enabled)
-
-        # Store the attributes/names in each object
-        self._robot_names = dir(self)
-        self._drive_train_names = dir(self._drive_train)
-        self._user_interface_names = dir(self._user_interface)
 
     def load_parameters(self):
         """Load values from a parameter file and create and initialize objects.
@@ -205,8 +183,6 @@ class MyRobot(wpilib.SimpleRobot):
     def _operator_control_init(self):
         """Prepares the robot for Teleop mode."""
         # Perform initialization before looping
-        if self._timer:
-            self._timer.stop()
 
         self._set_robot_state(common.ProgramState.TELEOP)
 
@@ -250,21 +226,6 @@ class MyRobot(wpilib.SimpleRobot):
 
             wpilib.Wait(0.01)
 
-    def reset_and_start_timer(self):
-        """Resets and restarts the timer."""
-        if self._timer:
-            self._timer.stop()
-            self._timer.start()
-
-    def wait_time(self, duration):
-        """Autonomous method that does nothing for a specified duration."""
-        elapsed_time = self._timer.elapsed_time_in_secs()
-        time_left = duration - elapsed_time
-        if time_left < 0:
-            self._timer.stop()
-            return True
-        return False
-
     def _read_sensors(self):
         """Have the objects read their sensors."""
         if self._drive_train:
@@ -276,16 +237,6 @@ class MyRobot(wpilib.SimpleRobot):
             self._drive_train.set_robot_state(state)
         if self._user_interface:
             self._user_interface.set_robot_state(state)
-
-    def _check_tele_auto_kill(self):
-        """Check kill switch for all tele-auto functionality."""
-        if (self._user_interface.get_button_state(
-                        userinterface.UserControllers.SCORING,
-                        userinterface.JoystickButtons.BACK) == 1):
-            self._hold_to_shoot_step = -1
-            self._prep_for_feed_step = -1
-            self._truss_pass_step = -1
-            self._aim_at_target_step = -1
 
     def _check_alternate_speed_modes(self):
         """Check for alternate speed mode."""
@@ -307,17 +258,6 @@ class MyRobot(wpilib.SimpleRobot):
             # Swap the driver controls to be the opposite of what they were
             self._driver_controls_swap_ratio = (self._driver_controls_swap_ratio
                                                 * -1.0)
-            # Print the notification for several seconds
-            self._disable_range_print = True
-            self._range_print_timer.start()
-            if self._driver_controls_swap_ratio > 0:
-                self._user_interface.output_user_message(("Controls "
-                                                          "normal"),
-                                                         True)
-            else:
-                self._user_interface.output_user_message(("Controls "
-                                                          "swapped"),
-                                                         True)
 
     def _control_drive_train(self):
         """Manually control the drive train."""
@@ -328,17 +268,12 @@ class MyRobot(wpilib.SimpleRobot):
                 userinterface.UserControllers.DRIVER,
                 userinterface.JoystickAxis.RIGHTX)
         if driver_left_y != 0.0 or driver_right_x != 0.0:
-            # Abort any relevent teleop auto routines
             if self._drive_train:
                 driver_left_y = driver_left_y * self._driver_controls_swap_ratio
                 self._drive_train.arcade_drive(driver_left_y, driver_right_x,
                                                False)
-                self._aim_at_target_step = -1
         else:
-            # Make sure we don't mess with any teleop auto routines
-            # if they're running
-            if self._aim_at_target_step < 0:
-                self._drive_train.arcade_drive(0.0, 0.0, False)
+            self._drive_train.arcade_drive(0.0, 0.0, False)
 
 
 def run():
